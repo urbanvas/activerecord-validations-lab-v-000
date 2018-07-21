42e1ba926cd15f0cc6785b5f39bd5172debb5d44
Neil Seejoor <nvseejoor@gmail.com>
2018-07-21 19:49:33 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :is_clickbait?
 
   CLICKBAIT = ["Won't Believe"]
 
