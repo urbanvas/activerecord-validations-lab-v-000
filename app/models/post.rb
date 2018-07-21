9730bde047ec0dc7daaac967aec709976bc7ddd7
Neil Seejoor <nvseejoor@gmail.com>
2018-07-21 19:47:52 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validate :is_clickbait?
 
-   CLICKBAIT_PATTERNS = [/Won't Believe/i]
+   CLICKBAIT_PATTERNS = [/Won't Believe/]
 
    def is_clickbait?
      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
